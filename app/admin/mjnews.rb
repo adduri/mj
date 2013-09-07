ca5104cae203233f7a65faeafaf4edcea7dfd682
bhanu <bhanu@Z580.(none)>
2013-09-07 18:11:38 +0530

    admin mjnews

--- app/admin/mjnews.rb
+++ app/admin/mjnews.rb
@@ -12,11 +12,11 @@ ActiveAdmin.register Mjnews do
      column :title
      column :content
      column :location
-     
+     default_actions
      column "Title" do |post|
     link_to image_tag(post.photo.url, :height => '150', :width => '150'),
     admin_mjnews_path(post)
-    default_actions
+    
 end
  end    
 end
