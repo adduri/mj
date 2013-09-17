a9b7f44ddc691bc6f3715b3e4e94fcf786d8171e
adduri <sagarkache1@gmail.com>
2013-09-17 14:59:29 +0530

    added late&admin user form

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register User do
+   form do |f|                       
+    f.inputs "User New" do
+
   
+    f.input :mj_id
+    f.input :email
+    f.input :firstname
+    f.input :lastname
+    f.input :gender , collection: ["Male","Female"]
+    f.input :ccode
+    f.input :mobileno
+    f.input :dob
+    f.input :station
+    f.input :city
+    f.input :zip
+    f.input :address 
+    f.input :state   
+   
+  end
+  f.buttons
+end
+ 
 end
